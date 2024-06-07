88732872e16f6bef03b17f352eb52ea8907ba614
Harsha N Savanth <Harsha.NSavanth@cognizant.com>
2024-06-08 01:04:20 +0530

    Added entities

---
+++ src/main/java/com/microservice/beerorderservice/domain/BeerOrderLine.java
@@ -0,0 +1,39 @@
+package com.microservice.beerorderservice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-01-26.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BeerOrderLine extends BaseEntity {
+
+    @Builder
+    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                         Integer quantityAllocated) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerOrder = beerOrder;
+        this.beerId = beerId;
+        this.orderQuantity = orderQuantity;
+        this.quantityAllocated = quantityAllocated;
+    }
+
+    @ManyToOne
+    private BeerOrder beerOrder;
+
+    private UUID beerId;
+    private Integer orderQuantity = 0;
+    private Integer quantityAllocated = 0;
+}
